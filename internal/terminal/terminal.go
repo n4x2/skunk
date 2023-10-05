291d73e1fd2ef653c5f2e977fece7d0d422a4047
Alfan Mubarok <alfan@smolhub.com>
2023-10-05 14:38:25 +0700

    internal/terminal: Add input and confirmation functions

--- internal/terminal/terminal.go
+++ internal/terminal/terminal.go
@@ -1,7 +1,47 @@
 // Package terminal provides utilities for terminal-related tasks.
 package terminal
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"golang.org/x/term"
+)
+
+// AskConfirmation returns true if it's "y" or "Y", otherwise false.
+func AskConfirmation() bool {
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		answer := scanner.Text()
+		if answer == "y" || answer == "Y" {
+			return true
+		}
+	}
+
+	return false
+}
+
+// AskValue reads from stdin and returns it as a string.
+func AskValue() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		return scanner.Text()
+	}
+
+	return ""
+}
+
+// AskCredentials reads a password from the terminal without echoing it,
+// returns the entered password as a string.
+func AskCredentials() (string, error) {
+	secret, err := term.ReadPassword(int(os.Stdin.Fd()))
+	if err != nil {
+		return "", err
+	}
+
+	return string(secret), nil
+}
 
 // ClearLines clears specified lines in terminal.
 func ClearLines(num int) {
